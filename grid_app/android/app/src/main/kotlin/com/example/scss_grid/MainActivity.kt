add3cc4726f117a798e7fecc2bf6819f089f1f29
xianfeng.xiong <xianfeng.xiong@ctechm.com>
2026-06-21 20:43:00 +0400

    chore: scaffold grid_app Flutter project with pdf dep

---
+++ grid_app/android/app/src/main/kotlin/com/example/scss_grid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scss_grid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ grid_app/test/widget_test.dart
@@ -0,0 +1,6 @@
+// Placeholder test file. Real tests live in test/model, test/grid, test/controls, test/pdf.
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('placeholder', () {});
+}
