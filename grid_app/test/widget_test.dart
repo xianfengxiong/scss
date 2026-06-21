b246f5719accaa92efed71353f9e4e873572eb56
xianfeng.xiong <xianfeng.xiong@ctechm.com>
2026-06-21 20:47:05 +0400

    cleanup: remove placeholder widget_test.dart

--- grid_app/test/widget_test.dart
+++
@@ -1,6 +0,0 @@
-// Placeholder test file. Real tests live in test/model, test/grid, test/controls, test/pdf.
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('placeholder', () {});
-}
